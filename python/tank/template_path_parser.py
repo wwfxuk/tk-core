ced48219357f2b2f622a919649a5c15b5afead1f
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-06-14 18:58:33 +0100

    yapf

--- python/tank/template_path_parser.py
+++ python/tank/template_path_parser.py
@@ -7,14 +7,35 @@
 # By accessing, using, copying or modifying this work you indicate your
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
-
 """
 Parsing of template paths into values for specified keys using a list of static tokens
 """
-
+from collections import namedtuple
 import os
 from .errors import TankError
 from .log import LogManager
+import logging
+
+
+"""
+Container class used to store possible resolved values during template
+parsing.  Stores the possible value as well as the downstream hierarchy
+of possible values, the last error found whilst parsing and a flag
+to specify if any of the branches in the downstream hierarchy are fully
+resolved (a value was found for every remaining key)
+
+:var value:                 The resolved value to keep track of
+:var downstream_values:     ResolvedValue instances for all possible downstream branches of
+                            possible resolved values
+:var fully_resolved:        Flag to track if any of the downstream branches are fully resolved
+                            or not
+:var last_error:            The last error reported from the template parsing for the current
+                            branch of possible values.
+"""
+ResolvedValue = namedtuple(
+    'ResolvedValue',
+    ['value', 'downstream_values', 'fully_resolved', 'last_error'],
+)
 
 
 class TemplatePathParser(object):
@@ -22,31 +43,6 @@ class TemplatePathParser(object):
     Class for parsing a path for a known set of keys, and known set of static
     tokens which should appear between the key values.
     """
-
-    class ResolvedValue(object):
-        """
-        Container class used to store possible resolved values during template
-        parsing.  Stores the possible value as well as the downstream hierarchy
-        of possible values, the last error found whilst parsing and a flag
-        to specify if any of the branches in the downstream hierarchy are fully
-        resolved (a value was found for every remaining key)
-        """
-        def __init__(self, value, downstream_values, fully_resolved, last_error):
-            """
-            Construction
-
-            :param value:               The resolved value to keep track of
-            :param downstream_values:   ResolvedValue instances for all possible downstream branches of
-                                        possible resolved values
-            :param fully_resolved:      Flag to track if any of the downstream branches are fully resolved
-                                        or not
-            :param last_error:          The last error reported from the template parsing for the current
-                                        branch of possible values.
-            """
-            self.value = value
-            self.downstream_values = downstream_values
-            self.fully_resolved = fully_resolved
-            self.last_error = last_error
 
     def __init__(self, ordered_keys, static_tokens):
         """
@@ -62,6 +58,9 @@ class TemplatePathParser(object):
         self.input_path = None
         self.last_error = "Unable to parse path"
         self.logger = LogManager.get_logger(self.__class__.__name__)
+        file_handler = logging.FileHandler('/home/joseph/repos/tk-core/var/parser.log')
+        file_handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(file_handler)
 
     def parse_path(self, input_path, skip_keys):
         """
@@ -142,16 +141,20 @@ class TemplatePathParser(object):
             if not positions:
                 # didn't find token!
                 self.last_error = ("Tried to extract fields from path '%s', "
-                                   "but the path does not fit the template." % input_path)
+                                   "but the path does not fit the template." %
+                                   input_path)
                 return None
             token_positions.append(positions)
 
         # disgard positions that can't be valid - e.g. where the position is greater than the
         # last possible position of any subsequent tokens:
-        max_position = len(lower_path)+1
+        max_position = len(lower_path) + 1
         for ti in reversed(range(len(token_positions))):
-            token_positions[ti] = [p for p in token_positions[ti] if p < max_position]
-            max_position = max(token_positions[ti]) if token_positions[ti] else 0
+            token_positions[ti] = [
+                p for p in token_positions[ti] if p < max_position
+            ]
+            max_position = max(
+                token_positions[ti]) if token_positions[ti] else 0
 
         # find all possible values for keys based on token positions - this will
         # return a list of lists including all potential variations:
@@ -163,13 +166,12 @@ class TemplatePathParser(object):
             #    t-k-t
             #    t-k-t-k
             #    t-k-t-k-k
-            if (num_keys >= num_tokens-1):
-                possible_values.extend(self.__find_possible_key_values_recursive(input_path,
-                                                                                 len(self.static_tokens[0]),
-                                                                                 self.static_tokens[1:],
-                                                                                 token_positions[1:],
-                                                                                 self.ordered_keys,
-                                                                                 skip_keys))
+            if (num_keys >= num_tokens - 1):
+                possible_values.extend(
+                    self.__find_possible_key_values_recursive(
+                        input_path, len(self.static_tokens[0]),
+                        self.static_tokens[1:], token_positions[1:],
+                        self.ordered_keys, skip_keys))
 
             # we've handled this case so remove the first position:
             token_positions[0] = token_positions[0][1:]
@@ -181,18 +183,17 @@ class TemplatePathParser(object):
             #    k-t-k-t
             #    k-t-k-k
             if (num_keys >= num_tokens):
-                possible_values.extend(self.__find_possible_key_values_recursive(input_path,
-                                                                                 0,
-                                                                                 self.static_tokens,
-                                                                                 token_positions,
-                                                                                 self.ordered_keys,
-                                                                                 skip_keys))
+                possible_values.extend(
+                    self.__find_possible_key_values_recursive(
+                        input_path, 0, self.static_tokens, token_positions,
+                        self.ordered_keys, skip_keys))
 
         if not possible_values:
             # failed to find anything!
             if not self.last_error:
                 self.last_error = ("Tried to extract fields from path '%s', "
-                                   "but the path does not fit the template." % input_path)
+                                   "but the path does not fit the template." %
+                                   input_path)
             return None
 
         # ensure that we only have a single set of valid values for all keys.  If we don't
@@ -214,23 +215,30 @@ class TemplatePathParser(object):
                 possible_values = possible_values[0].downstream_values
             else:
                 # found more than one possible value so check to see how many are fully resolved:
-                resolved_possible_values = [v for v in possible_values if v.fully_resolved]
+                resolved_possible_values = [
+                    v for v in possible_values if v.fully_resolved
+                ]
                 num_resolved = len(resolved_possible_values)
 
                 if num_resolved == 1:
                     # only found one resolved value - awesome!
                     key_value = resolved_possible_values[0].value
-                    possible_values = resolved_possible_values[0].downstream_values
+                    possible_values = resolved_possible_values[
+                        0].downstream_values
                 elif num_resolved > 1:
                     # found more than one valid value so value is ambiguous!
-                    self.last_error = ("Ambiguous values found for key '%s' could be any of: '%s'"
-                                       % (key.name, "', '".join([v.value for v in resolved_possible_values])))
+                    self.last_error = (
+                        "Ambiguous values found for key '%s' could be any of: '%s'"
+                        % (key.name, "', '".join(
+                            [v.value for v in resolved_possible_values])))
                     return None
                 else:
                     # didn't find any fully resolved values so we have multiple
                     # non-fully resolved values which also means the value is ambiguous!
-                    self.last_error = ("Ambiguous values found for key '%s' could be any of: '%s'"
-                                       % (key.name, "', '".join([v.value for v in possible_values])))
+                    self.last_error = (
+                        "Ambiguous values found for key '%s' could be any of: '%s'"
+                        % (key.name, "', '".join(
+                            [v.value for v in possible_values])))
                     return None
 
             # if key isn't a skip key then add it to the fields dictionary:
@@ -240,8 +248,14 @@ class TemplatePathParser(object):
         # return the single unique set of fields:
         return fields
 
-    def __find_possible_key_values_recursive(self, path, key_position, tokens, token_positions,
-                                             keys, skip_keys, key_values=None):
+    def __find_possible_key_values_recursive(self,
+                                             path,
+                                             key_position,
+                                             tokens,
+                                             token_positions,
+                                             keys,
+                                             skip_keys,
+                                             key_values=None):
         """
         Recursively traverse through the tokens & keys to find all possible values for the keys
         given the available token positions im the path.
@@ -276,7 +290,7 @@ class TemplatePathParser(object):
             # make sure that the length of the possible value substring will be valid:
             if token_position <= key_position:
                 continue
-            if key.length is not None and token_position-key_position < key.length:
+            if key.length is not None and token_position - key_position < key.length:
                 continue
 
             # get the possible value substring:
@@ -291,50 +305,49 @@ class TemplatePathParser(object):
                 # slashes are not allowed in key values!  Note, the possible value is a section
                 # of the input path so the OS specific path separator needs to be checked for:
                 if os.path.sep in possible_value_str:
-                    last_error = ("%s: Invalid value found for key %s: %s"
-                                  % (self, key.name, possible_value_str))
+                    last_error = ("%s: Invalid value found for key %s: %s" %
+                                  (self, key.name, possible_value_str))
                     continue
 
                 # can't have two different values for the same key:
                 if key_value and possible_value_str != key_value:
-                    last_error = ("%s: Conflicting values found for key %s: %s and %s"
-                                  % (self, key.name, key_value, possible_value_str))
+                    last_error = (
+                        "%s: Conflicting values found for key %s: %s and %s" %
+                        (self, key.name, key_value, possible_value_str))
                     continue
 
                 # get the actual value for this key - this will also validate the value:
                 try:
+                    self.logger.warning('Resolving "%s" using "%s"', key.name, possible_value_str)
                     possible_value = key.value_from_str(possible_value_str)
                 except TankError as e:
                     # it appears some locales are not able to correctly encode
                     # the error message to str here, so use the %r form for the error
                     # (ticket 24810)
-                    last_error = ("%s: Failed to get value for key '%s' - %r"
-                                  % (self, key.name, e))
+                    last_error = ("%s: Failed to get value for key '%s' - %r" %
+                                  (self, key.name, e))
                     continue
 
             else:
                 # don't bother doing validation/conversion for this value as it's being skipped!
                 possible_value = possible_value_str
 
+            self.logger.warning('--> "%s"', possible_value)
             downstream_values = []
             fully_resolved = False
             if keys:
                 # still have keys to process:
-                if token_position+len(token) >= len(path):
+                if token_position + len(token) >= len(path):
                     # but we've run out of path!  This is ok
                     # though - we just stop processing keys...
                     fully_resolved = True
                 else:
                     # have keys remaining and some path left to process so recurse to next position for next key:
-                    downstream_values = self.__find_possible_key_values_recursive(path,
-                                                                       token_position+len(token),
-                                                                       tokens,
-                                                                       token_positions,
-                                                                       keys,
-                                                                       skip_keys,
-                                                                       dict(key_values.items()
-                                                                            + [(key.name, possible_value_str)])
-                                                                       )
+                    downstream_values = self.__find_possible_key_values_recursive(
+                        path, token_position + len(token), tokens,
+                        token_positions, keys, skip_keys,
+                        dict(key_values.items() +
+                             [(key.name, possible_value_str)]))
 
                     # check that at least one of the returned values is fully
                     # resolved and find the last error found if any
@@ -348,7 +361,7 @@ class TemplatePathParser(object):
             elif tokens:
                 # we don't have keys but we still have remaining tokens - this is bad!
                 fully_resolved = False
-            elif token_position+len(token) != len(path):
+            elif token_position + len(token) != len(path):
                 # no keys or tokens left but we haven't fully consumed the path either!
                 fully_resolved = False
             else:
@@ -356,9 +369,8 @@ class TemplatePathParser(object):
                 fully_resolved = True
 
             # keep track of the possible values:
-            possible_values.append(TemplatePathParser.ResolvedValue(possible_value,
-                                                                    downstream_values,
-                                                                    fully_resolved,
-                                                                    last_error))
+            possible_values.append(
+                ResolvedValue(possible_value, downstream_values,
+                              fully_resolved, last_error))
 
         return possible_values
